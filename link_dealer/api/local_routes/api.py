c4ca928825a6b2277d859b5ddf094691868e8c08
Vcslav <vcslav.v@gmail.com>
2022-02-08 20:26:37 +0300

    make utm

--- link_dealer/api/local_routes/api.py
+++ link_dealer/api/local_routes/api.py
@@ -1,14 +1,22 @@
 import os
 import secrets
+from datetime import datetime
+from urllib.parse import urlparse
+import json
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
+from link_dealer import schemas
 
 router = APIRouter()
 security = HTTPBasic()
 
 username = os.environ.get('API_USERNAME', 'root')
 password = os.environ.get('API_PASSWORD', 'pass')
+subscription_url = os.environ.get('SUB_URL', 'subscription_url')
+main_url = os.environ.get('MAIN_URL', 'main_url')
+utm_cattegories = json.loads(os.environ.get('UTM_CATS', '{}'))
 
 
 def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
@@ -23,6 +31,29 @@ def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
     return credentials.username
 
 
-@router.get('/test')
-def test(_: str = Depends(get_current_username)):
-    pass
+@router.post('/make-utm')
+def make_utm(utm_info: schemas.utm_info, _: str = Depends(get_current_username)) -> schemas.utms:
+    category = urlparse(utm_info.link)[2].split('/')[-2].replace('-', '')
+    term = f'{utm_info.item_type}-item-{category}'
+    dt = datetime.utcnow().date()
+    result = schemas.utms()
+    utm_source = f'utm_source={utm_cattegories[utm_info.source]["source"]}'
+    utm_medium = f'utm_medium={utm_cattegories[utm_info.source]["medium"]}'
+    utm_campaign = f'utm_campaign={utm_info.project}-{dt.strftime("%Y%m%d")}'
+
+    for content_settings in utm_cattegories[utm_info.source]['content']:
+        url = utm_info.link
+        utm_content = 'utm_content=text'
+        if content_settings == 'to_subscription':
+            url = subscription_url
+        elif content_settings == 'to_main':
+            url = main_url
+        else:
+            utm_content = f'utm_content={content_settings}'
+        result.utms.append(
+            schemas.utm(
+                desc=content_settings,
+                link=url + '&'.join([utm_source, utm_medium, utm_campaign, utm_content, term])
+            )
+        )
+    return result
---
+++ link_dealer/schemas.py
@@ -0,0 +1,32 @@
+from typing import Optional
+from pydantic import BaseModel, validator
+import json
+import os
+
+
+class utm_info(BaseModel):
+    link: str
+    source: str
+    project: str
+    item_type: str
+
+    @validator('source')
+    def check_source(cls, source):
+        if source in json.loads(os.environ.get('UTM_CATS', '{}')):
+            return source
+        raise ValueError('Not valide site')
+
+    @validator('item_type')
+    def check_project(cls, item_type):
+        if item_type not in ['premium', 'freebie', 'plus']:
+            raise ValueError('Not valide item_type')
+        return item_type
+
+
+class utm(BaseModel):
+    desc: str
+    link: str
+
+
+class utms(BaseModel):
+    utms: list[utm] = []
